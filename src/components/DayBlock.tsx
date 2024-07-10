0cde1ad64a74cbb20e36abc9d1291cb1eadd374e
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-07-10 17:36:54 +0200

    ajout popup

--- src/components/DayBlock.tsx
+++ src/components/DayBlock.tsx
@@ -5,28 +5,31 @@ interface DayBlockProps {
   date: Date;
   index: number;
 }
+
 interface HourBlockProps {
   hour: number;
+  date: Date; 
   index: number;
 }
 
-const DayBlock: React.FC<DayBlockProps> = () => {
+const DayBlock: React.FC<DayBlockProps> = ({ date, index }) => {
   const hours = [];
   for (let i = 8; i <= 20; i++) {
     hours.push(i);
   }
+
   return (
     <div className="text-center ml-4 ">
-      <div className=" h-full mb-0 grid grid-cols-1 grid-rows-12 gap-4  ">
+      <div className="h-full mb-0 grid grid-cols-1 grid-rows-12 gap-4">
         {hours.map((hour, hourIndex) => (
-          <HourBlock key={hourIndex} hour={hour} index={hourIndex} />
+          <HourBlock key={`${index}-${hourIndex}`} hour={hour} date={date} index={hourIndex} />
         ))}
       </div>
     </div>
   );
 };
 
-const HourBlock: React.FC<HourBlockProps> = () => {
+const HourBlock: React.FC<HourBlockProps> = ({ hour, date }) => {
   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
 
   const handleClick = () => {
@@ -39,7 +42,7 @@ const HourBlock: React.FC<HourBlockProps> = () => {
         className="w-full bg-opacity-50 bg-[#2A2B34] rounded-xl pb-16 transform transition-all duration-300 hover:scale-110 hover:bg-opacity-100 cursor-pointer z-10"
         onClick={handleClick}
       ></div>
-      {isPopupOpen ? <Poppup /> : ""}
+      {isPopupOpen && <Poppup date={date} hour={hour} handleClose={(value: boolean) => setIsPopupOpen(value)} />}
     </>
   );
 };
--- src/components/Poppup.tsx
+++ src/components/Poppup.tsx
@@ -1,30 +1,75 @@
+import React from "react";
 
+interface PopupI {
+  date: Date;
+  hour: number;
+  handleClose: (close: boolean) => void;
+}
 
+const Popup: React.FC<PopupI> = (props) => {
+  const formatDate = (date: Date) => {
+    const formattedDate = date.toLocaleDateString("fr-FR", {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+    return formattedDate;
+  };
 
+  const formatTime = (hour: number) => {
+    return `${hour.toString().padStart(2, "0")}:00`; // Convert to HH:MM format
+  };
 
-const Poppup = () => {
+  const handleCancel = () => {
+    props.handleClose(false);
+  };
+
+  const handleConfirm = () => {};
+
   return (
-    <div className='w-full h-full fixed top-0 left-0 bg-black bg-opacity-15 flex items-center justify-center z-50'>
-      <div className="w-1/2 bg-[#2A2B34] border border-white rounded-lg overflow-hidden">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="w-full max-w-lg bg-[#2A2B34]  border border-gray-700 rounded-lg shadow-lg overflow-hidden">
         {/* Header */}
-        <div className="flex justify-between items-center p-4 border-b border-white">
+        <div className="flex justify-between items-center p-4 border-b border-gray-700">
           <h2 className="text-xl font-bold text-white">Ajouter un Évènement</h2>
-    
         </div>
         {/* Body */}
-        <div className="p-4 text-white">
-
+        <div className="p-6">
+          <div className="text-white mb-4">
+            <div className="mb-2">
+              <span className="font-bold">{formatDate(props.date)}</span> à <span className="font-bold">{formatTime(props.hour)}</span>
+            </div>
+            <div className="mb-2">
+              <input
+                className="w-full px-3 py-2 border border-gray-600 rounded bg-gray-700 text-white focus:outline-none focus:border-blue-500"
+                type="text"
+                id="eventTitle"
+                placeholder="Titre de l'évènement"
+              />
+            </div>
+            <div>
+              <input
+                className="w-full px-3 py-2 pb-9 border border-gray-600 rounded bg-gray-700 text-white focus:outline-none focus:border-blue-500"
+                type="text"
+                id="eventDescription"
+                placeholder="Description de l'évènement"
+              />
+            </div>
+          </div>
         </div>
         {/* Footer */}
-        <div className="flex justify-end p-4 border-t border-white">
-          <button 
-            
-            className="mr-2 bg-gray-500 text-white px-4 py-2 rounded">
+        <div className="flex justify-end p-4 border-t border-gray-700">
+          <button
+            className="mr-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-700"
+            onClick={handleCancel}
+          >
             Annuler
           </button>
-          <button 
-         
-            className="bg-blue-500 text-white px-4 py-2 rounded">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded"
+            onClick={handleConfirm}
+          >
             Confirmer
           </button>
         </div>
@@ -33,4 +78,4 @@ const Poppup = () => {
   );
 };
 
-export default Poppup;
+export default Popup;
