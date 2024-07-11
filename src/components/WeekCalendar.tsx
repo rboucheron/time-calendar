b9b8e1f04c913fed76439cfd6b196ee6e31b8cb1
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-07-11 23:03:27 +0200

    update

--- src/components/WeekCalendar.tsx
+++ src/components/WeekCalendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import ChangeView from "./ChangeView";
 import DayBlock from "./DayBlock";
 import ChangeWeek from "./ChangeWeek";
